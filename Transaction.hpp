ab16e15516b783231c6902545ba33bb39b94120f
Euclideus <143367005+Euclidae@users.noreply.github.com>
2025-03-06 22:33:50 +0200

    added some adjustments to the transactions header.
    Make sure you apply them appropriate to the implementation.

--- Transaction.hpp
+++ Transaction.hpp
@@ -1,6 +1,5 @@
 #ifndef TRANSACTION_HPP
 #define TRANSACTION_HPP
-#include <stdint.h>
 #include <iostream>
 #include <string>
 /// @brief
@@ -13,8 +12,7 @@ class Transaction{
         bool is_expense;
 
     public:
-        Transaction(std::string t_id, std::string n_date, std::string n_category, double n_amount, bool n_is_expense);
-        Transaction(int64_t t_id, std::string n_date, std::string n_category, double n_amount, bool n_is_expense);
+        explicit Transaction(std::string t_id, std::string n_date, std::string n_category, double n_amount, bool n_is_expense);
         Transaction();
         std::string getDate();
         std::string getCategory();
@@ -27,6 +25,8 @@ class Transaction{
         void setCategory(std::string n_category);
         void setAmount(double n_amount);
         void setTransactionType(bool n_is_expense);
+        Transaction& operator = (const Transaction&) = delete;
+        Transaction(const Tranction&) = delete;
         ~Transaction();
 
 };
